3d3375fa0dcdabf3b4fe5ecf30f629db3d9ae613
Chloe Gell <gellert.chloe@gmail.com>
2020-02-06 21:37:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,12 @@ class Scraper
   def get_page 
   end 
   
+  def get_courses 
+  end 
+  
+  def make_course 
+  end 
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
